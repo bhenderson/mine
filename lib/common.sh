fd54cf3150ee9118f858757e09b6a2e4e59a1ea1
bhenderson <bhenderson@attinteractive.com>
2011-08-13 22:09:10 -0700

    Got 3rd FD working instead of temp file!!!

--- lib/common.sh
+++ lib/common.sh
@@ -1,4 +1,5 @@
 export rubies_path=$mine_path/rubies
+export orig_ruby="$mine_ruby"
 
 abort() {
   printf "%s\n" "$@" >&2
@@ -6,16 +7,25 @@ abort() {
 }
 
 list_rubies() {
-  opt="${1-.}"
+  opt="$1"
 
   pat() {
-    echo "$opt" | sed 's/./&*/g'
+    echo "$opt" | sed 's/./&.*/g'
   }
 
   # list all directories
   (
-    cd $rubies_path
-    find * -prune -name `pat`
+    cd "$rubies_path"
+    ls | grep "`pat`"
   )
 }
 
+update_ruby() {
+  [[ "$orig_ruby" == "$mine_ruby" ]] && return
+
+cat >&3 <<-EOS
+  export mine_ruby="$mine_ruby"
+  export GEM_HOME="$mine_path/rubies/$mine_ruby/gems"
+  export GEM_PATH="$mine_path/rubies/$mine_ruby/gems"
+EOS
+}
--- lib/list.sh
+++ lib/list.sh
@@ -1,4 +1,9 @@
 option="$1"
 
-list_rubies "$1"
-
+rubies=("`list_rubies "$1"`")
+(
+  cd "$mine_path"
+  for ruby in ${rubies[@]}; do
+    ls -ld "rubies/$ruby" | cut -d/ -f2
+  done
+)
